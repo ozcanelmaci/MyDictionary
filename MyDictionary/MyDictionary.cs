76214de1c0efbd62f6c6d14583e69985903acf30
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2021-01-22 20:56:29 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V>
+    {
+        List<K> listOfKey;
+        List<V> listOfValue;
+
+        public MyDictionary()
+        {
+            listOfKey = new List<K>();
+            listOfValue = new List<V>();
+        }
+
+        public void Add(K key, V value)
+        {
+            if (!listOfKey.Contains(key))
+            {
+                listOfKey.Add(key);
+                listOfValue.Add(value);
+            }
+        }
+
+        public int Count
+        {
+            get { return listOfKey.Count; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            dictionary.Add("2018510023", "Özcan Elmacı");
+            dictionary.Add("1234567891", "İsim1 Soyisim1");
+            dictionary.Add("2345678912", "İsim2 Soyisim2");
+
+            Console.WriteLine(dictionary.Count);
+
+
+            //MyDictionary 
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+
+            myDictionary.Add("2018510023", "Özcan Elmacı");
+            myDictionary.Add("1234567891", "İsim1 Soyisim1");
+            myDictionary.Add("2345678912", "İsim2 Soyisim2");
+            myDictionary.Add("2345678912", "İsim2 Soyisim2"); //this is for trying whether MyDictionary allows to add the same Key or not.
+
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
